77933910eeba8ffc255f8926262ea5c5afc74133
Derek McCauley <derekjmccauley@gmail.com>
2025-04-30 21:15:15 -0400

    Corrected Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   
 
   const handleBuyPro = () => {
-    window.open('https://orchidsolutions.gumroad.com/l/ColorMatchrPro', '_blank')
+    window.open('https://orchidsolutions.gumroad.com/l/ColorMatchrPro?wanted=true', '_blank')
     // Uncomment below line for local testing
     // setHasPaid(true)
   }
